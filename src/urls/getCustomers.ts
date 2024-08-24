6fdfac7d68f988abd38ec0ebe7cd50e41c8c7636
Artag-Chris <scristxyz@gmail.com>
2024-08-24 11:17:13 -0500

    clonando bot de meta

---
+++ src/urls/getCustomers.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+
+export const getPhones = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/whatsapp/getphones');
+      console.log('Response data:', response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error('Error message:', error.message);
+      } else {
+        console.error('Unexpected error:', error);
+      }
+    }
+  };
+  
+  getPhones();
