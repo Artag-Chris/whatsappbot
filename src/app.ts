5d8274a23850e7d3929581ecee949e7b49f442d7
Artag-Chris <scristxyz@gmail.com>
2024-08-26 16:16:36 -0500

    comentando

--- src/app.ts
+++ src/app.ts
@@ -63,20 +63,20 @@ function main(){
     res.sendStatus(200);
   });
   
-  // accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
-  // info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+  // solo acepta requests de /webhook endpoint.
+  // info sobr el request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
   app.get("/webhook", (req, res) => {
     const mode = req.query["hub.mode"];
     const token = req.query["hub.verify_token"];
     const challenge = req.query["hub.challenge"];
   
-    // check the mode and token sent are correct
+    
     if (mode === "subscribe" && token === envs.WEBHOOK_VERIFY_TOKEN) {
-      // respond with 200 OK and challenge token from the request
+      
       res.status(200).send(challenge);
       console.log("Webhook verified successfully!");
     } else {
-      // respond with '403 Forbidden' if verify tokens do not match
+      
       res.sendStatus(403);
     }
   });
@@ -87,7 +87,7 @@ function main(){
   });
   
   app.listen(envs.PORT, () => {
-    console.log(`Server is listening on port: ${envs.PORT}`);
+    console.log(`bot en puerto: ${envs.PORT}`);
   });
 
 }
