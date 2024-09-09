9a504374dbba5b68e18a4fa1293547765dbeff64
Artag-Chris <scristxyz@gmail.com>
2024-09-09 16:19:59 -0500

    arreglado el problema del doble payload

--- src/functions/sendMessageToApi.ts
+++ src/functions/sendMessageToApi.ts
@@ -1,11 +1,26 @@
 import axios, { AxiosResponse } from "axios";
-import {envs} from "../config/envs/envs";
+import { envs } from "../config/envs/envs";
+import crypto from "crypto";
+
+const sentPayloads = new Set<string>();
+
+function hashPayload(payload: any): string {
+    return crypto.createHash('sha256').update(JSON.stringify(payload)).digest('hex');
+}
 
 export async function sendMessageToApi(payload: any): Promise<AxiosResponse<any>> {
     const apiUrl = envs.URLCREATEMESSAGE;
+    const payloadHash = hashPayload(payload);
+
+    if (sentPayloads.has(payloadHash)) {
+        console.log('Payload ya enviado anteriormente:', payload);
+        throw new Error('Payload ya enviado anteriormente');
+    }
+
     try {
         const response = await axios.post(apiUrl, payload);
         console.log('Payload enviado a la API:', response.data);
+        sentPayloads.add(payloadHash);
         return response;
     } catch (error) {
         if (axios.isAxiosError(error)) {
